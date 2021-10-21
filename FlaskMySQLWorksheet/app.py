5f67ca822e2ce87d2ecbede0935b6274484685dd
BillieEyelash <riatalwar@hotmail.com>
2021-10-20 18:49:02 -0700

    Transfer SQL query to results and make it secure

--- FlaskMySQLWorksheet/app.py
+++ FlaskMySQLWorksheet/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -11,15 +11,22 @@ mysql = MySQL(app)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    name = request.form.get("searchname")
     # Set up MySQL object which can be used for multiple queries
     cursor = mysql.connection.cursor()
     # Define query string
-    query = 'SELECT * FROM riatalwar_test'
+    query = "SELECT * FROM riatalwar_test WHERE name=%s;"
+    # List of variables to insert into query
+    queryVars = (name,)
     # Execute (actually run) the predefined query
-    cursor.execute(query)
+    cursor.execute(query, queryVars)
     # Commit query (necessary when executing multiple queries)
     mysql.connection.commit()
     # Fetches all rows returned by the query (only necessary when getting information)
     # The data is returned as a multidimensional associative array
     data = cursor.fetchall()
-    return render_template('index.html', rows=data)
+    return render_template("results.html", numPeople=len(data))
