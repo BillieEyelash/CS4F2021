f4725a4157ec984d778a507086b90aa71baf086a
BillieEyelash <riatalwar@hotmail.com>
2021-11-04 09:12:29 -0700

    Add SQL queries

--- FlaskSQLProject/app.py
+++ FlaskSQLProject/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'mysql.2122.lakeside-cs.org'
+app.config['MYSQL_USER'] = 'student2122'
+app.config['MYSQL_PASSWORD'] = 'm545CS42122'
+app.config['MYSQL_DB'] = '2122playground'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -18,26 +25,72 @@ def results():
     ''' Description: Launch the results page and retrieve input from form
         Parameters: None
         Return: Webpage for form or results '''
-    inputName = request.form.get("firstname"), request.form.get("lastname")
+    # Retrieve form input
+    nameInput = request.form.get("firstname") + " " + request.form.get("lastname")
+    usernameInput = request.form.get("username")
     passwords = request.form.get("password"), request.form.get("passconfirm")
     passwordInput = passwords[0]
     emailInput = request.form.get("email")
     # Stay at form if input is invalid
-    if not valid_input(inputName, passwords, emailInput):
+    if not valid_input(nameInput, usernameInput, passwords, emailInput):
         return index()
-
+    # Get remaining inputs
     genreInput = request.form.getlist("genre")
-    booksReadInput = request.form.get("booksread")
-    return render_template('results.html', name=inputName, password=passwords[0], email=emailInput, genres=genreInput, booksRead=booksReadInput)
+    booksReadInput = int(request.form.get("booksread"))
+
+    cursor = mysql.connection.cursor()
+
+    # Insert data into table
+    query = "INSERT INTO `riatalwar_form` (`name`, `username`, `password`, `email`, `books_read`) VALUES (%s, %s, %s, %s, %s);"
+    queryVars = (nameInput, usernameInput, passwordInput, emailInput, booksReadInput)
+    execute_query(query, queryVars, cursor)
+
+    # Usernames have to be unique
+    query = "SELECT id FROM `riatalwar_form` WHERE username=%s;"
+    queryVars = (usernameInput,)
+    id = execute_query(query, queryVars, cursor)[0]['id']
+
+    for genre in genreInput:
+        # Get genre id if already in genre table
+        query = "SELECT id FROM `riatalwar_genres` WHERE genre=%s"
+        queryVars = (genre,)
+        idData = execute_query(query, queryVars, cursor)
+        # Add genre to table if necessary
+        if len(idData) == 0:
+            query = "INSERT INTO `riatalwar_genres` (`genre`) VALUES (%s)"
+            execute_query(query, queryVars, cursor)
+            # Get genre id
+            query = "SELECT id FROM `riatalwar_genres` WHERE genre=%s"
+            idData = execute_query(query, queryVars, cursor)
+        genreID = idData[0]['id']
+        # Add favorite genre to table
+        query = "INSERT INTO `riatalwar_favorite_genres` (`person_id`, `genre_id`) VALUES (%s, %s)"
+        queryVars = (id, genreID)
+        execute_query(query, queryVars, cursor)
+
+    query = "SELECT username FROM riatalwar_form WHERE NOT id=%s AND id IN (SELECT person_id FROM riatalwar_favorite_genres WHERE genre_id IN (SELECT genre_id FROM riatalwar_favorite_genres WHERE genre_id IN (SELECT genre_id FROM riatalwar_favorite_genres WHERE person_id=%s)))"
+    queryVars = (id, id)
+    personData = execute_query(query, queryVars, cursor)
+    people = set()
+    for p in personData:
+        people.add(p['username'])
+    return render_template('results.html', p=people)
 
 
-def valid_input(name, passwords, email):
+def valid_input(name, username, passwords, email):
     ''' Description: Determine whether or not the given inputs are valid
         Parameters: List [first, last name], List [password, confirm password], String email
         Return: Boolean name, password, and email are valid '''
     # Check if name is blank
     if name[0] == "" or name[1] == "":
         return False
+
+    cursor = mysql.connection.cursor()
+    query = "SELECT * FROM riatalwar_form WHERE username=%s"
+    queryVars = (username,)
+    if len(execute_query(query, queryVars, cursor)) != 0:
+        return False
+
     # Check if passwords don't match or are blank
     elif passwords[0] != passwords[1] or passwords[0] == "":
         return False
@@ -45,3 +98,12 @@ def valid_input(name, passwords, email):
     elif "@" not in email:
         return False
     return True
+
+
+def execute_query(query, queryVars, cursor):
+    ''' Description: Execute the given query
+        Parameters: String query, Tuple query variables, Cursor
+        Return: Query results '''
+    cursor.execute(query, queryVars,)
+    cursor.connection.commit()
+    return cursor.fetchall()
